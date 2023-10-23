9637554a0ca620e0fdbfd3374aa864331134eccb
Medayss <132765134+Medayss@users.noreply.github.com>
2023-10-23 14:05:09 +0900

    Delete 숙제풀이 directory

--- 숙제풀이/AbstractOperation.java
+++
@@ -1,5 +0,0 @@
-package 숙제풀이;
-
-public abstract class AbstractOperation {
-    public abstract double operate(int firstNumber, int secondNumber);
-}
--- 숙제풀이/SubstractOperation.java
+++
@@ -1,8 +0,0 @@
-package 숙제풀이;
-
-public class SubstractOperation extends AbstractOperation{
-    @Override
-    public double operate(int firstNumber, int secondNumber){
-        return firstNumber - secondNumber;
-    }
-}
